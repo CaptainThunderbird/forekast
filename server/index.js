1a0afdb9dcc6a232887773d68105505ff2a18a7c
CaptainThunderbird <lanagarcia03@gmail.com>
2026-07-11 00:25:16 +0800

    updated index

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./auth.routes');
@@ -6,15 +8,12 @@ const tweetsRoutes = require('./tweets.routes');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // Essential for parsing incoming req.body JSON data
+app.use(express.json());
 
-// API Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/tweets', tweetRoutes);
+app.use('/api/tweets', tweetsRoutes);
 
-// Base Health Check
 app.get('/', (req, res) => {
   res.send('Forekast backend server is running smoothly!');
 });
